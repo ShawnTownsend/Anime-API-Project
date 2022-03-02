3008b30dbaab0ff145d9d69e92b671832c441b05
Mislam23 <islam.mohammad.student@gmail.com>
2022-03-02 10:19:36 -0500

    added character selection div for page 2

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//DISAPPEAR HOME PAGE BACKGROUND
+document.body.style.backgroundImage = "url('images/backgrounds/naruto-background.png')";
+
+//FETCH QUOTE
 fetch("https://animechan.vercel.app/api/quotes/character?name=naruto")
 .then(response => response.json())
 .then(data => console.log(data))
