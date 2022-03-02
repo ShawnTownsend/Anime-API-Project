ecaf872fca5e5a3c890b14e5d14a4050b23dec31
Shawn Townsend <fr3sh9475@hotmail.com>
2022-03-02 11:25:50 -0500

    Changes

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-//DISAPPEAR HOME PAGE BACKGROUND
-document.body.style.backgroundImage = "url('images/backgrounds/naruto-background.png')";
-
 //FETCH QUOTE
 fetch("https://animechan.vercel.app/api/quotes/character?name=naruto")
 .then(response => response.json())
 .then(data => console.log(data))
 
+const dbzLogo = document.getElementById('dbz-logo');
+const onePieceLogo = document.getElementById('one-piece-logo');
+const narutoLogo = document.getElementById('naruto-logo');
 
+narutoLogo.addEventListener('click', narutoSelect);
+
+/*
+Once logo is clicked, make homepage disappear.
+Go to character selection page for anime show.
+Add event listeners to each logo.
+Inside event listener: Invoke showCharacter function.
+*/
+
+function narutoSelect() {
+    document.body.style.backgroundImage = "url('images/backgrounds/naruto-background.png')";
+}
