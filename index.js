22288abfe2a47c980065fbe794c1ad48da1d6b3e
raylu511 <raylu511@gmail.com>
2022-03-02 10:19:42 -0500

    idk

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ fetch("https://animechan.vercel.app/api/quotes/character?name=naruto")
 .then(data => console.log(data))
 
 
+document.body.style.backgroundImage = "url('images/backgrounds/dbz-background.jpg')";
